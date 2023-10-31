ef56e8f0264fa05ee44d3e5477a70766b1aa5ad3
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-10-31 23:36:31 +0000

    Display random quote to DOM

--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
-console.log("working");
+let random_quote = document.getElementById("random-quote");
+let author_section = document.getElementById("author-section");
+let author = document.getElementById("author");
+let genre = document.getElementById("genre");
+
 const BASE_URL = "https://quote-garden.onrender.com";
+let is_loading = true;
+
+window.addEventListener("load", async ()=>{
+    console.log("windows loaded");
+    displayQuote();
+})
 
 
 const fetchData = async (url)=>{
@@ -9,5 +19,17 @@ const fetchData = async (url)=>{
     return data;
 }
 
+const displayQuote = async ()=>{
+   let data =  await fetchData(BASE_URL+"/api/v3/quotes/random");
+   console.log("DATA", data);
 
-fetchData(BASE_URL+"/api/v3/quotes/random");
+   data.data.forEach(ele => {
+        console.log(ele);
+        random_quote.innerText = ele.quoteText;
+        author.innerText = ele.quoteAuthor;
+        genre.innerText = ele.quoteGenre;
+   });
+}
+
+
+
