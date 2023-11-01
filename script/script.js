47d6e31e7633973a426adc7971af5138eca07319
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-11-01 00:28:57 +0000

    Remove loading screen when data is returned from the API on initial loading of the app

--- script/script.js
+++ script/script.js
@@ -3,9 +3,12 @@ let author_section = document.getElementById("author-section");
 let author = document.getElementById("author");
 let genre = document.getElementById("genre");
 let next_btn = document.getElementById("next-btn");
+let loading_screen = document.getElementById("loading-screen");
+let quote_section = document.getElementById("quote-section");
+let author_quotes_section = document.getElementById("author-quotes-section")
 
 const BASE_URL = "https://quote-garden.onrender.com";
-let is_loading = true;
+
 
 window.addEventListener("load", async ()=>{
     console.log("windows loaded");
@@ -22,8 +25,15 @@ const fetchData = async (url)=>{
 }
 
 const displayQuote = async ()=>{
+    
    let data =  await fetchData(BASE_URL+"/api/v3/quotes/random");
-   console.log("DATA", data);
+   console.log("DATA", data.statusCode);
+
+   if(data.statusCode === 200){
+    loading_screen.classList.add("d-none");
+    quote_section.classList.remove("d-none");
+   }
+   
 
    data.data.forEach(ele => {
         console.log(ele);
